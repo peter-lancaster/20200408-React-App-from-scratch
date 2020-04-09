d8be3810650f9afbd38b19ff2d4ed9f09a69b4db
peter-lancaster <lancasterp2@hotmail.co.uk>
2020-04-09 10:58:53 +0100

    first commit for this project

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>This is my header - now amended</header>
+    )
+}
+
+
+export default Header
---
+++ src/components/MainContent.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+function MainContent() {
+
+    return (<main>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <input type="checkbox"></input><span>_____________________________________</span>
+        <br />
+        <br />
+        <br />
+    </main>)
+
+}
+
+
+export default MainContent
+
+
